f0f44a3353fcbcd2a0bc96286ab1f68ef99fc467
Daniel Lindner <daniel.lindner@softwareschneiderei.de>
2020-04-06 15:35:51 +0200

    Code des initialen Projekts (Überschreiben/Überladen)

---
+++ src/de/dhbw/tinf17b3/intro/Animal.java
@@ -0,0 +1,12 @@
+package de.dhbw.tinf17b3.intro;
+
+public abstract class Animal {
+	
+	public Animal() {
+		super();
+	}
+
+	public void streichle() {
+		System.out.println("S1: Lass mich!");
+	}
+}
---
+++ src/de/dhbw/tinf17b3/intro/Main.java
@@ -0,0 +1,31 @@
+package de.dhbw.tinf17b3.intro;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class Main {
+	
+	private Main() {
+		super();
+	}
+	
+	public static void main(String[] args) {
+		final Animal bernhard = new Cat();
+		final Animal james = new Staubsauger();
+		
+		bernhard.streichle();
+		james.streichle();
+		
+		final List<Animal> zoo = new ArrayList<>();
+		zoo.add(bernhard);
+		zoo.add(james);
+		
+		zoo.forEach(Animal::streichle);
+		
+		final Kitchen kitchen = new Kitchen();
+		kitchen.füttere(bernhard);
+		kitchen.füttere(james);
+		
+		zoo.forEach(kitchen::füttere);
+	}
+}
